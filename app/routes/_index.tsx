5a28ba37f8dc9c33c5cdee184c5c813d9282ec95
tonsorialdev <147235191+tonsorialdev@users.noreply.github.com>
2025-02-13 23:43:12 -0800

    Additional UI + mail modal

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
+import {Form} from "@remix-run/react";
+import {useState} from "react";
+import {Modal} from "antd";
+
 export const meta: MetaFunction = () => {
   return [
     { title: "Danjo" },
@@ -7,15 +11,30 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
  return (
 <>
     <div className="homepage-container">
-        <img className="background-image" src="../../public/windows9999.jpeg" alt="background"/>
             <div className="works">
                 <button>
                     <img className="folder-icon" src="../../public/folder.png" alt="projects"/>
                     <span style={{color: "white", fontSize: ".5em"}}> PROJECTS </span>
                 </button>
+
         </div>
             <div className="resume">
                     <button>
@@ -25,26 +44,35 @@ export default function Index() {
 
             </div>
         <div className="dock">
-
         </div>
+
+
+        <Modal title="" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+            <Form className="email-form" method="POST">
+            <input className="email-field email-from" type="email" placeholder="Email" required/>
+            <input className="email-field email-subject" type="subject" placeholder="subject" required/>
+            <textarea className="email-field email-from" placeholder={"Message"}></textarea>
+            </Form>
+        </Modal>
+
+
         <div className={"dock-items"}>
-            <button className="github-button dock-button">
-                <img src="../../public/github-mark.svg" alt="background"/>
-            </button>
-            <button className="linkedin-button dock-button">
+        <a href="https://github.com/danj-jo" className="github-button dock-button">
+                <img src="../../public/github-mark-white.svg" alt="background"/>
+            </a>
+            <a href="https://www.linkedin.com/in/danj-jo/" className="linkedin-button dock-button">
                 <img src="../../public/linkedin.png" alt="background"/>
-            </button>
-            <button className="linkedin-button dock-button">
+            </a>
+            <button className="gmail-button dock-button" onClick={showModal}>
                 <img src="../../public/gmail.png" alt="background"/>
             </button>
-            <button className="linkedin-button dock-button">
+            <button className="music-button dock-button">
                 <img src="../../public/player.png" alt="background"/>
             </button>
         </div>
 
 
     </div>
-
 
 </>
 
